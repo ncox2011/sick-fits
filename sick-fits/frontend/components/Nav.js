81e4e17f0e1e7a24b095fee70cf855dff5d7e572
Natasha Cox <natashac1014@gmail.com>
2018-12-14 13:40:07 -0600

    changed { to (

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,32 +1,39 @@
 import Link from 'next/link';
 import User from './User';
 import NavStyles from './styles/NavStyles';
+import Signout from './Signout';
 
 const Nav = () => (
-    <NavStyles>
     <User>
-        {({data: {me}}) => {
-            console.log(me);
-            if (me) return <p>{me.name}</p>;
-            return null;
-        }}
+        {({ data: { me } }) => (
+            <NavStyles>
+                <Link href="/items">
+                    <a>Shop</a>
+                </Link>
+                {me && (
+                    <>
+                        <Link href="/sell">
+                            <a>Sell</a>
+                        </Link>
+                        <Link href="/orders">
+                            <a>Orders</a>
+                        </Link>
+                        <Link href="/me">
+                            <a>Account</a>
+                        </Link>
+                        <Signout />
+                    </>
+                )}
+                {!me && (
+
+                    <Link href="/signup">
+                        <a>Sign In</a>
+                    </Link>
+                )}
+
+            </NavStyles>
+        )}
     </User>
-        <Link href="/items">
-        <a>Shop</a>
-        </Link>
-        <Link href="/sell">
-        <a>Sell</a>
-        </Link>
-        <Link href="/signup">
-        <a>Signup</a>
-        </Link>
-        <Link href="/orders">
-        <a>Orders</a>
-        </Link>
-        <Link href="/me">
-        <a>Account</a>
-        </Link>
-    </NavStyles>
 )
 
 export default Nav;
